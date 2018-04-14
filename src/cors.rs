3c916f23ebd9a6ec8831bd91fa1a42760ba8522d
Akhil kp <akhilkpdasan@gmail.com>
2018-04-14 19:46:41 +0530

    Send wildcard on CORS response

--- src/cors.rs
+++ src/cors.rs
@@ -3,10 +3,8 @@ use actix_web::middleware::cors;
 
 pub fn options() -> cors::Cors {
     cors::Cors::build()
-        .allowed_origin("http://localhost:3000")
-        .allowed_methods(vec!["DELETE", "GET", "POST"])
-        .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
-        .allowed_header(header::CONTENT_TYPE)
+        .send_wildcard()
+        .allowed_methods(vec!["DELETE", "GET", "POST", "PUT"])
         .max_age(3600)
         .finish().expect("Can not create CORS middleware")
 }
