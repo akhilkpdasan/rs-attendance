c946222b6539089f92fd5b2e3e9db5363ddebbb8
Akhil kp <akhilkpdasan@gmail.com>
2018-04-30 12:39:07 +0530

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,13 @@ fn main() {
     let sys = actix::System::new("example");
     ::std::env::set_var("RUST_LOG", "actix_web=info");
     ::std::env::set_var("RUST_BACKTRACE", "1");
-    
     env_logger::init();
+
     server::new(create_app)
         .bind("192.168.0.103:8088")
         .expect("Couldn't bind server to addrsess")
         .start();
-        
+
     println!("Started http server: 127.0.0.1:8088");
     let _ = sys.run();
 }
