8b474f31eb6b37261c425935072daf38c2683ea4
Akhil kp <akhilkpdasan@gmail.com>
2018-05-26 00:38:36 +0530

    increase timeout in axios [ci skip]

--- webapp/src/api.js
+++ webapp/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const config = axios.create({
   baseURL: 'http://localhost:8088',
-  timeout: 5000,
+  timeout: 10000,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
